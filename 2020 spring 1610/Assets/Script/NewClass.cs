896133e1b3e0a7f62547c7c249e7af1e0cdac2f7
Will lemmons <will.e.lemmons@gmail.com>
2020-01-13 11:15:49 -0700

    New class

---
+++ 2020 spring 1610/Assets/Script/NewClass.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewClass : MonoBehaviour
+{
+    public int cash;
+    //whole numbers
+    public float coin;
+    //fraction numbers
+    public string money;
+    //words
+    public bool rich;
+    //true or false
+
+    // Start is called before the first frame update
+    void Start()
+    //Nothing Whatever
+    {
+        print("Nothing Whatever");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("NOTHING WHATEVER!!!!!!!!!!!!!!");
+    }
+}
