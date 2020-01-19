b58b8c9d436188df07b4da33d51e1d1617a36384
Will lemmons <will.e.lemmons@gmail.com>
2020-01-19 11:57:06 -0700

    Double Jump

--- 2020 spring 1610/Assets/Script/Controller.cs
+++ 2020 spring 1610/Assets/Script/Controller.cs
@@ -7,8 +7,7 @@ public class Controller : MonoBehaviour
     public float playerSpeed;
     public float jumpPower;
     public float jumpFall;
-    public bool playerMove;
-    public bool playerJump;
+    public bool grounded;
     public bool snowGoggles;
     private Rigidbody myRigidBody;
     private Vector3 move;
@@ -18,30 +17,39 @@ public class Controller : MonoBehaviour
     {
         myRigidBody = GetComponent<Rigidbody>();
     }
-
+    
     // Update is called once per frame
     void Update()
     {
+        //move input
         move = new Vector3(Input.GetAxisRaw("Horizontal"),0f, Input.GetAxisRaw("Vertical"));
 
-        
-        if(Input.GetButtonDown("Jump"))
+        //jump
+        if(Input.GetButtonDown("Jump") && grounded)
         {
             myRigidBody.velocity = Vector3.up * jumpPower;
+            grounded = false;
         }
+        //fall
         if(myRigidBody.velocity.y < 0)
         {
             myRigidBody.velocity += Vector3.up * Physics.gravity.y * (jumpFall - 1) * Time.deltaTime;
         }
     }
-    
+    //FixedUpdate is called every physics update
     void FixedUpdate()
     {
         Movement(move);
     }
-
+    //move
     void Movement(Vector3 direction)
     {
         myRigidBody.MovePosition(transform.position + (direction * playerSpeed * Time.deltaTime));
     }
+    
+    void OnCollisionStay()
+    {
+        grounded = true;
+    }
+
 }
