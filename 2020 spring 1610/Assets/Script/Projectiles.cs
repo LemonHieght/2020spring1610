0b37b42acf6d1f47f2623bd620e19bb555bee316
Will lemmons <will.e.lemmons@gmail.com>
2020-01-29 17:25:50 -0700

    projectile start

--- 2020 spring 1610/Assets/Script/Projectiles.cs
+++ 2020 spring 1610/Assets/Script/Projectiles.cs
@@ -1,11 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectiles : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject bullet;
+    public float speed;
+    private Vector3 velo;
+    private Rigidbody bulletRigidbody;
+
+    private void Start()
     {
         
     }
@@ -13,6 +18,11 @@ public class Projectiles : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            GameObject instBullet = Instantiate(bullet, transform.position, Quaternion.identity) as GameObject;
+            Rigidbody instBulletRigidbody = instBullet.GetComponent<Rigidbody>();
+            instBulletRigidbody.AddForce(Vector3.forward * speed);
+        }
     }
 }
---
+++ 2020 spring 1610/Assets/Script/TriggerEvents.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEvents : MonoBehaviour
+{
+    public int whol;
+    public float desa;
+    public string words;
+    public UnityEvent triggerExit;
+    public UnityEvent triggerEnter;
+    public UnityEvent triggerStay;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnter.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        triggerExit.Invoke();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        triggerStay.Invoke();
+    }
+}
