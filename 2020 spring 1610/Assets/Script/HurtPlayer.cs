48e3dea665102658b38ef02858cc4837a41e52a8
Will lemmons <will.e.lemmons@gmail.com>
2020-04-23 20:51:39 -0800

    health and enemy attack

---
+++ 2020 spring 1610/Assets/Script/HurtPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HurtPlayer : MonoBehaviour
+{
+    public int damageGiven;
+    public UnityEvent triggerEnter;
+    private void OnCollisionEnter(Collision other)
+    {
+        triggerEnter.Invoke();
+    }
+}
