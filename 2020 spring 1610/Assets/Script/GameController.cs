89f4029ef9620338b4762ab1d0c59b5363ae9808
Will lemmons <will.e.lemmons@gmail.com>
2020-04-27 10:59:06 -0800

    main menu finished

---
+++ 2020 spring 1610/Assets/Script/GameController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    private bool gameHasEnded = false;
+    private bool gameHasWon = false;
+    public GameObject gameOverUI;
+    public GameObject gameWonUI;
+    public GameObject howToPlay;
+    public GameObject mainMenu;
+    public IntData score;
+
+    void start()
+    {
+        gameHasWon = false;
+    }
+    public void WonGame()
+    {
+        if (gameHasWon == false)
+        {
+            gameHasWon = true;
+            Debug.Log("game complete");
+            Victory();
+        }
+    }
+    public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game Over");
+            GameOver();
+            score.value = 0;
+
+        }
+    }
+    void GameOver()
+    {
+        gameOverUI.SetActive(true);
+    }
+    public void HowTo()
+    {
+        howToPlay.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+    void Victory()
+    {
+        gameWonUI.SetActive(true);
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void MainMenu()
+    {
+        mainMenu.SetActive(true);
+        howToPlay.SetActive(false);
+    }
+    public void MenuStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
